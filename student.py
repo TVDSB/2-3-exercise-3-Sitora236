963a40c7541f57cd5dcb4c1ea9ba31397f10d194
Sitora236 <100421722+Sitora236@users.noreply.github.com>
2022-03-29 09:25:16 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,6 +1,13 @@
 def main():
     #your code goes here
-    print("0")
-
+    num = int(input("Enter a number:"))
+    if num %3 == 0 and num %5 == 0:
+      print("fizzbuzz")
+    elif num % 5 == 0:
+      print("buzz")
+    elif num %3 == 0 :
+      print("fizz")
+    else:
+      print(num)
 if __name__ =='__main__':
     main()
